e7e7653867f3cb5ec0921b35542ecc3fce4dd5ee
Dovakin95 <Nepstor95@gamil.com>
2023-01-17 17:03:04 +0300

    доблены категори отзывы смартфоны

---
+++ models/Categories.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const categorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    image: String,
+   
+  },
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
---
+++ models/Smartphones.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+
+
+
+const smarthoneShema = mongoose.Schema({
+    wirelessInterfaces: String, // Беспроводные интерфейсы
+    numberOfSimCards: Number,  // Количество SIM карт
+    operatingSystemVersion: String, // Версия операционной системы
+    model: String, // Модель
+    screenDiagonal: Number, // Диагональ экрана
+    screenResolution:String, //  Разрешение экрана
+    numberProcessorCores:Number, // Количество ядер процессора
+    cpu:String, // Процессор
+    builtInMemory:Number, // Объем встроенной памяти
+    ram:Number, // Объем оперативной памяти
+    memoryCardType:String, // Тип карты памяти
+    batteryCapacity:String, // Емкость аккумулятора
+    
+
+    image:String,
+    name:String,
+    decription:String,
+    price:Number,
+    discount:Number,
+    category:{
+        type:mongoose.SchemaTypes.ObjectId,
+        ref:"Category"
+    },
+    reviews: [
+        {
+            user:{
+                type:mongoose.SchemaTypes.ObjectId,
+                ref:"User",
+            },
+            text:String,
+        },
+    ],
+});
+
+const Smarthone = mongoose.model("Smarthone",smarthoneShema)
+
+module.exports = Smarthone
